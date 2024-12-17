8816a17b7de6328cee148106f00e3c712ca08e7e
VenilSutariya_ <135502144+venilsutariya@users.noreply.github.com>
2024-12-17 12:08:28 +0530

    complete

--- app/page.tsx
+++ app/page.tsx
@@ -20,16 +20,16 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="grid grid-cols-3 h-[100vh] items-center gap-x-[100px] p-5 bg-gradient-to-t from-pink-50 via-white to-purple-50">
-      <div data-cursor-text="Hello!" className="h-[300px] text-xl font-[500] shadow-lg rounded-3xl border border-black flex justify-center items-center">
+    <div className="flex justify-center items-center h-[100vh] gap-x-[100px] p-5 bg-gradient-to-t from-pink-100 via-white to-purple-100">
+      <div data-cursor-text="Hello!" className="h-[300px] w-[500px] text-xl font-[500] shadow-lg rounded-3xl border border-black flex justify-center items-center">
         Hover me to see text
       </div>
-      <div data-cursor-img="https://www.pacegallery.com/media/images/16_9-2.width-2000.png" className="h-[300px] text-xl font-[500] shadow-lg rounded-3xl border border-black flex justify-center items-center">
+      {/* <div data-cursor-img="https://www.pacegallery.com/media/images/16_9-2.width-2000.png" className="h-[300px] text-xl font-[500] shadow-lg rounded-3xl border border-black flex justify-center items-center">
         Hover me to see image
-      </div>
-      <div data-cursor-video="/video.mp4" className="h-[300px] text-xl font-[500] shadow-lg rounded-3xl border border-black flex justify-center items-center">
+      </div> */}
+      {/* <div data-cursor-video="/video.mp4" className="h-[300px] text-xl font-[500] shadow-lg rounded-3xl border border-black flex justify-center items-center">
         Hover me to see video
-      </div>
+      </div> */}
     </div>
   );
 }
